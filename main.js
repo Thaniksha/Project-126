ef7ec04889fe8943a0dbc3fab3206f7eff8d902d
Thaniksha <73345437+Thaniksha@users.noreply.github.com>
2022-05-10 15:55:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1=""
 song2=""
+rightWristX="";
+rightWristY="";
+leftWristX="";
+leftWristY="";
 
 function preload(){
         song1=loadSound("Stay.mp3");
@@ -12,12 +16,34 @@ function setup(){
         
              video=createCapture(VIDEO)
              video.hide()
+
+             PoseNet=ml5.poseNet(video,modelLoaded);
+             PoseNet.on("pose",gotPoses);
          }
+
+         function modelLoaded(){
+            console.log("Model Loaded Successfully")
+        }
+       
+        function gotPoses(results){
+       if(results.length>0){
+           rightWristX=results[0].pose.rightWrist.x;
+           rightWristY=results[0].pose.rightWrist.y;
+           console.log("RightWristX=" +rightWristX +"RightWristY="+rightWristY);
+       
+           leftWristX=results[0].pose.leftWrist.x;
+           leftWristY=results[0].pose.leftWrist.y;
+           console.log("LeftWristX="+leftWristX+"LeftWristY="+leftWristY);
+       }
+    }       
  function draw(){
              image(video,0,0,450,450);
         
          }
  function play(){
-             songs.play()
+             song1.play()
+             song1.setVolume(1);
+             song1.rate(1);
+             
          }
      
